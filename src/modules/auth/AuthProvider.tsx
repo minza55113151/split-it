c4f7c26170617f35c60dd55e2d5038d82183618c
Chatuporn <minza1131@outlook.com>
2025-03-25 02:05:15 +0700

    Provider

---
+++ src/modules/auth/AuthProvider.tsx
@@ -0,0 +1,41 @@
+import { useAuth } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
+import apiInstance from "../services/myApi";
+import { useUser } from "@/hooks/UseUser";
+import { useUserRegister } from "@/hooks/UseUserRegister";
+
+const AuthProvider = () => {
+  const [token, setToken] = useState<string>("");
+
+  const { data: user, isLoading } = useUser(token !== "");
+  const { mutate: userRegister } = useUserRegister();
+
+  const { getToken } = useAuth();
+
+  useEffect(() => {
+    getToken().then((token) => {
+      if (!token) {
+        return;
+      }
+
+      apiInstance.instance.interceptors.request.use((config) => {
+        config.headers.Authorization = `Bearer ${token}`;
+        return config;
+      });
+      setToken(token);
+    });
+  }, [getToken]);
+
+  useEffect(() => {
+    if (!token || isLoading || user) {
+      return;
+    }
+
+    console.log("Registering user");
+    userRegister();
+  }, [token, user, isLoading, userRegister]);
+
+  return null;
+};
+
+export default AuthProvider;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,21 @@ import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import DebugComponent from "@/modules/DebugComponent";
+import AuthProvider from "@/modules/auth/AuthProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+export const queryClient = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ClerkProvider>
-      <>
-        <DebugComponent />
-        <Component {...pageProps} />
-      </>
+      <QueryClientProvider client={queryClient}>
+        <>
+          <DebugComponent />
+          <AuthProvider />
+          <Component {...pageProps} />
+        </>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 }
