ae93b540d2f7de1b877f583de39020c9faa6b244
Chatuporn <minza1131@outlook.com>
2025-03-31 20:43:03 +0700

    Add max h

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -16,6 +16,7 @@ function Input({
         "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 flex min-w-0 bg-transparent px-3 py-1 text-base transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         border && "focus-visible:border-ring border-b focus-visible:border-b-2",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        "max-h-[33px]",
         className,
       )}
       {...props}
